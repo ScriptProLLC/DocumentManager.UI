20ca318b3ab050780c4a2cbb1a4df62c3c17a882
Luke Balsiger <lbalsiger@scriptpro.com>
2019-03-28 14:00:26 -0500

    Merged PR 5831: Adding linting stuff
    Adding linting stuff
    
    Husky, prettier and lint-staged
    
    Related work items: #138651

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,14 +1,13 @@
 /// <reference types="Cypress" />
 
-describe('App', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
-  
-    // https://on.cypress.io/interacting-with-elements
-  
-    it('starts correctly', () => {
-        cy.get('p').should('have.text', 'Edit src/App.js and save to reload.')
-    })
-  })
-  
+describe("App", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it("starts correctly", () => {
+    cy.get("p").should("have.text", "Edit src/App.js and save to reload.");
+  });
+});
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import {render, fireEvent, getByLabelText} from 'react-testing-library';
-import App from './App'
+import React from "react";
+import { render, fireEvent, getByLabelText } from "react-testing-library";
+import App from "./App";
 
-it('runs a basic test', () => {
+it("runs a basic test", () => {
   expect(2 + 2).toBe(4);
 });
 
-it('renders the app component', () => {
-  const {container} = render(<App/>)
-  expect(container.querySelector('p').textContent).toBe('Edit src/App.js and save to reload.')
-})
+it("renders the app component", () => {
+  const { container } = render(<App />);
+  expect(container.querySelector("p").textContent).toBe(
+    "Edit src/App.js and save to reload."
+  );
+});
