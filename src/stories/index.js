6ae667657bba8d666cf692aedca4059ffc13a55c
John Cresswell <jcresswell@scriptpro.com>
2019-03-29 11:08:46 -0500

    Merged PR 5851: info addon now playing nicely with knobs addon
    info addon now playing nicely with knobs addon
    
    Related work items: #138747

--- src/stories/index.js
+++ src/stories/index.js
@@ -7,24 +7,40 @@ import { linkTo } from "@storybook/addon-links";
 import { Button, Welcome } from "@storybook/react/demo";
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
+import { withInfo } from "@storybook/addon-info";
+
 const stories = storiesOf("Storybook Knobs", module);
 
 // Add the `withKnobs` decorator to add knobs support to your stories.
 // You can also configure `withKnobs` as a global decorator.
 stories.addDecorator(withKnobs);
 
+stories.addDecorator(
+  withInfo({
+    header: false // Global configuration for the info addon across all components
+  })
+);
+
 // Knobs for React props
-stories.add("with a button", () => (
-  <button disabled={boolean("Disabled", false)}>
-    {text("Label", "Hello Storybook")}
-  </button>
-));
+stories.add(
+  "with a button",
+  () => (
+    <button disabled={boolean("Disabled", false)}>
+      {text("Label", "Hello Storybook")}
+    </button>
+  ),
+  { info: "button info" }
+);
 
 // Knobs as dynamic variables.
-stories.add("as dynamic variables", () => {
-  const name = text("Name", "Arunoda Susiripala");
-  const age = number("Age", 89);
+stories.add(
+  "as dynamic variables",
+  () => {
+    const name = text("Name", "Arunoda Susiripala");
+    const age = number("Age", 89);
 
-  const content = `I am ${name} and I'm ${age} years old.`;
-  return <div>{content}</div>;
-});
+    const content = `I am ${name} and I'm ${age} years old.`;
+    return <div>{content}</div>;
+  },
+  { info: "this is a dynamic variable" }
+);
