4b79593e76afb909f0e4234d6d07096de8c009a5
Luke Balsiger <lbalsiger@scriptpro.com>
2019-04-11 14:10:36 -0500

    Merged PR 5983: Added theming
    Added theming
    
    Related work items: #141047

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,18 @@
 import { configure } from "@storybook/react";
 import "@storybook/addon-console";
+import { themes } from "@storybook/theming";
+import { addParameters } from "@storybook/react";
 
 const req = require.context("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+});
 
 configure(loadStories, module);
