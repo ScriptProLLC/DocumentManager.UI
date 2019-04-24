bfdc8df4fc55f2b9ac8b66e547160ecda1adcf68
Luke Balsiger <lbalsiger@scriptpro.com>
2019-04-24 13:18:01 -0500

    Merged PR 6126: basic framework/placeholders for integration tests
    basic framework/placeholders for integration tests
    
    Related work items: #143367

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /// <reference types="Cypress" />
 
 describe("App", () => {
@@ -7,7 +8,17 @@ describe("App", () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it("starts correctly", () => {
-    cy.get("h1").should("have.text", "Scriptpro Document Manager");
+  it("document list header should have correct text", () => {
+    var header = cy.get("[data-testid=document_list_header]");
+
+    expect(header).to.exist;
   });
+
+  it("document list scroll bar is functional", () => {});
+
+  it("built in functions of pdf viewer work", () => {});
+
+  it("selecting a document in the list renders the pdf", () => {});
+
+  it("clicking on a document in the list selects the document", () => {});
 });
