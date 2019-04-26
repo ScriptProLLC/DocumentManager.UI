ded19d9ddaff807532acef653310105219503c42
Russell Bronston <rbronston@scriptpro.com>
2019-04-26 09:37:57 -0500

    Merged PR 6133: Refactor of PDF Viewer with pattern
    Refactor of PDF Viewer with pattern
    
    Related work items: #143400

--- src/components/DocumentViewer/DocumentViewer.js
+++ src/components/DocumentViewer/DocumentViewer.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import pdfjsLib from "pdfjs-dist";
 
+import DocumentViewerViewer from "./DocumentViewerViewer";
+import { DocumentViewerContext } from "./DocumentViewerContext";
 import "bootstrap/dist/css/bootstrap.css";
 import "./DocumentViewer.css";
 
@@ -13,63 +15,30 @@ function DocumentViewer(props) {
   let viewport = null;
 
   pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js`;
+  const [pages, setPages] = useState(null);
+  const viewPort = useRef(null);
+
+  var doc = props.document
+    ? { data: atob(props.document.documentFile) }
+    : "ScriptProDefault.pdf";
 
   useEffect(() => {
-    var doc = props.document
-      ? { data: atob(props.document.documentFile) }
-      : "ScriptProDefault.pdf";
-
-    viewport = document.querySelector("#viewport");
     window.initPDFViewer(doc);
-  });
+    //window.initPDFViewer(PDF_URL);
+  }, [pdfInstance]);
 
   window.initPDFViewer = function(pdfURL) {
     pdfjsLib.getDocument(pdfURL).then(pdf => {
       pdfInstance = pdf;
       totalPagesCount = pdf.numPages;
-      initPager();
       render();
     });
   };
 
-  function onPagerButtonsClick(event) {
-    const action = event.target.getAttribute("data-pager");
-    if (action === "prev") {
-      if (currentPageIndex === 0) {
-        return;
-      }
-      currentPageIndex -= 1;
-      if (currentPageIndex < 0) {
-        currentPageIndex = 0;
-      }
-      render();
-    }
-    if (action === "next") {
-      if (currentPageIndex === totalPagesCount - 1) {
-        return;
-      }
-      currentPageIndex += 1;
-      if (currentPageIndex > totalPagesCount - 1) {
-        currentPageIndex = totalPagesCount - 1;
-      }
-      render();
-    }
-  }
-  function initPager() {
-    const pager = document.querySelector("#pager");
-    pager.addEventListener("click", onPagerButtonsClick);
-    return () => {
-      pager.removeEventListener("click", onPagerButtonsClick);
-    };
-  }
-
   function render() {
     cursorIndex = Math.floor(currentPageIndex);
     const startPageIndex = cursorIndex;
-    const endPageIndex =
-      startPageIndex + 1 < totalPagesCount
-        ? startPageIndex + 1 - 1
-        : totalPagesCount - 1;
+    const endPageIndex = totalPagesCount - 1;
 
     const renderPagesPromises = [];
     for (let i = startPageIndex; i <= endPageIndex; i++) {
@@ -77,20 +46,23 @@ function DocumentViewer(props) {
     }
 
     Promise.all(renderPagesPromises).then(pages => {
-      const pagesHTML = `<div style="width: ${"100%"}"><canvas></canvas></div>`.repeat(
-        pages.length
-      );
-      viewport.innerHTML = pagesHTML;
+      setPages(pages);
+      console.log("ViewPort ", viewport);
       pages.forEach(renderPage);
     });
   }
 
   function renderPage(page) {
     let pdfViewport = page.getViewport(1);
+    //console.log("in the renderpage");
 
-    const container = viewport.children[page.pageIndex - cursorIndex];
+    const container = viewPort.current.children[page.pageIndex - cursorIndex];
+    //console.log("Container ", container);
+
     pdfViewport = page.getViewport(container.offsetWidth / pdfViewport.width);
+    //console.log("pfdViewport ", pdfViewport);
     const canvas = container.children[0];
+    //console.log("Canvas", canvas);
     const context = canvas.getContext("2d");
     canvas.height = pdfViewport.height;
     canvas.width = pdfViewport.width;
@@ -102,17 +74,9 @@ function DocumentViewer(props) {
   }
 
   return (
-    <div id="app">
-      <div role="toolbar" id="toolbar">
-        <div id="pager">
-          <button data-pager="prev">prev</button>
-          <button data-pager="next">next</button>
-        </div>
-      </div>
-      <div id="viewport-container">
-        <div role="main" id="viewport" />
-      </div>
-    </div>
+    <DocumentViewerContext.Provider value={[pages, viewPort]}>
+      <DocumentViewerViewer />
+    </DocumentViewerContext.Provider>
   );
 }
 
