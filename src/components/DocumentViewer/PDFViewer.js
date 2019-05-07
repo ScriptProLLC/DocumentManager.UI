101150beb97fcaf7e6cde44bca610176ec3aa96d
Travis Gillitzer <tgillitzer@scriptpro.com>
2019-05-07 14:29:32 -0500

    Merged PR 6243: Update MSI to distribute PDFJS Viewer
    Update MSI to distribute PDFJS Viewer
    
    Related work items: #144840

--- src/components/DocumentViewer/PDFViewer.js
+++ src/components/DocumentViewer/PDFViewer.js
@@ -1,13 +1,11 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { DocumentViewerContext } from "./DocumentViewerContext";
-import PdfJsLib from "pdfjs-dist";
 import { decodeBase64 } from "../../util/Base64Util";
 
 function PDFViewer() {
   let iFrameRef = useRef();
   const [isFrameLoaded, setIsFrameLoaded] = useState(false);
 
-  PdfJsLib.GlobalWorkerOptions.workerSrc = "pdfjs-dist/pdf.worker.js";
   const { documentFile } = useContext(DocumentViewerContext);
 
   useEffect(() => {
@@ -24,7 +22,7 @@ function PDFViewer() {
     <iframe
       title="pdfjs"
       className="pdfjs"
-      src="/pdfjs-2.0.943-dist/web/viewer.html?file="
+      src="./pdfjs-2.0.943-dist/web/viewer.html?file="
       frameBorder="0"
       width="100%"
       height="100%"
