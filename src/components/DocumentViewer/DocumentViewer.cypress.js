9d0683f3537c109b8d98b739843d02f8e4dd89e6
John Cresswell <jcresswell@scriptpro.com>
2019-05-09 10:23:27 -0500

    Merged PR 6283: check in of remaining PDF Viewer integration test
    check in of remaining PDF Viewer integration test. Other tests moved to storybook/manual. Note this one was reviewed and pulled into the consolidated code in our Wednesday meeting. Since that code won't get checked in directly, this covers get that test into source control.
    
    Related work items: #140858

--- src/components/DocumentViewer/DocumentViewer.cypress.js
+++ src/components/DocumentViewer/DocumentViewer.cypress.js
@@ -8,7 +8,24 @@ describe("DocumentViewer", () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it("built in functions of pdf viewer work", () => {});
+  it("selecting a document in the list renders the pdf", () => {
+    //// should be at least one in list and a document rendered already
+    cy.get("[data-testid=documentListItems]").then($selectedElement => {
+      expect($selectedElement).to.be.not.null;
 
-  it("selecting a document in the list renders the pdf", () => {});
+      var elemToClick = $selectedElement[0].children[1].querySelector(
+        "[data-testid=collapse]"
+      );
+
+      elemToClick.click();
+
+      cy.get("[class=document-viewer-container]").then($selectedElement => {
+        expect($selectedElement).to.be.not.null;
+
+        expect($selectedElement[0].children[0].className.trim()).to.equal(
+          "pdfjs"
+        );
+      });
+    });
+  });
 });
