2578e672590035c5c00e59ba4d2ad2aa18a06a83
Cory House <housecor@gmail.com>
2019-05-11 12:45:03 -0500

    Fix knobs

--- src/components/ModalDialog/ModalDialog.stories.js
+++ src/components/ModalDialog/ModalDialog.stories.js
@@ -1,34 +1,31 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { storiesOf } from "@storybook/react";
-import { withKnobs, object } from "@storybook/addon-knobs";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 import ModalDialog from "./ModalDialog";
 
 function reportResult(result) {
   console.log(result);
 }
 
-// just a no-op for the toggle action as it has no real meaning here
-function toggle() {}
-
 storiesOf("ModalDialog", module)
   .addDecorator(withKnobs)
   .add("All params set", () => (
     <ModalDialog
-      showDialog={object("showDialog", true)}
-      toggle={toggle}
-      iconStyle={object("iconStyle", "Warning")}
-      cancelPrompt={object("cancelPrompt", "No")}
+      showDialog={boolean("showDialog", true)}
+      toggle={() => {}}
+      iconStyle={text("iconStyle", "Warning")}
+      cancelPrompt={text("cancelPrompt", "No")}
       reportResult={reportResult}
-      confirmPrompt={object("confirmPrompt", "Yes")}
-      header={object("header", "Alert")}
-      prompt={object("prompt", "Are you sure you want to do this?")}
+      confirmPrompt={text("confirmPrompt", "Yes")}
+      header={text("header", "Alert")}
+      prompt={text("prompt", "Are you sure you want to do this?")}
     />
   ))
   .add("Use all available defaults", () => (
     <ModalDialog
-      showDialog={object("showDialog", true)}
+      showDialog={boolean("showDialog", true)}
       reportResult={reportResult}
-      prompt={object("prompt", "Are you sure you want to do this?")}
+      prompt={text("prompt", "Are you sure you want to do this?")}
     />
   ));
