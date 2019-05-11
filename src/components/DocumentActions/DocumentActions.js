a791ced61422d65a24c2e77fd5406d4a491a001d
Cory House <housecor@gmail.com>
2019-05-11 13:49:18 -0500

    Add propTypes and default props

--- src/components/DocumentActions/DocumentActions.js
+++ src/components/DocumentActions/DocumentActions.js
@@ -1,16 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "font-awesome/css/font-awesome.min.css";
 import "./DocumentActions.css";
 
 function DocumentActions(props) {
-  let documentName =
-    props.document && props.document.name ? props.document.name : String.Empty;
-
   return (
     <div className="document-actions-container">
-      <span data-testid="documentActionDocumentName">{documentName}</span>
+      <span data-testid="documentActionDocumentName">
+        {props.document.name}
+      </span>
     </div>
   );
 }
+
+DocumentActions.propTypes = {
+  // would suggest just passing in documentName alone if that's the only property used in this component.
+  document: PropTypes.shape({
+    name: PropTypes.string
+  })
+};
+
+DocumentActions.defaultProps = {
+  document: { name: "" }
+};
 
 export default DocumentActions;
