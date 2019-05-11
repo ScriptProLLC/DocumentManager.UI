4dee4e2df326e8ec807c5504500c89982c3bcc8d
Cory House <housecor@gmail.com>
2019-05-11 10:10:33 -0500

    Rename

--- src/util/dateFormatter.test.js
+++
@@ -1,5 +0,0 @@
-import formatDate from "./formatDate";
-
-it("should format an ISO 8601 date in MM/DD/YYYY hh:mm a format", () => {
-  expect(formatDate("1997-01-01T06:30:16.25")).toEqual("01/01/1997 06:30 am");
-});
