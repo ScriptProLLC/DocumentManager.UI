bd769bf3d445d8d23818e84fcae84c216501daf5
Cory House <housecor@gmail.com>
2019-05-11 21:38:56 -0500

    Add propTypes and comments

--- src/components/DocumentActions/DocumentActions.js
+++ src/components/DocumentActions/DocumentActions.js
@@ -1,23 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { documentType } from "../../types";
 import "./DocumentActions.css";
 
 function DocumentActions(props) {
-  if (!props.document) return null;
+  // Why render this at all without a document? Suggest requiring document and only rendering when a document is available.
+  let documentName =
+    props.document && props.document.name ? props.document.name : String.Empty;
+
   return (
     <div className="document-actions-container">
-      <span data-testid="documentActionDocumentName">
-        {props.document.name}
-      </span>
+      <span data-testid="documentActionDocumentName">{documentName}</span>
     </div>
   );
 }
 
 DocumentActions.propTypes = {
-  // would suggest just passing in documentName alone if that's the only property used in this component.
-  document: PropTypes.shape({
-    name: PropTypes.string
-  })
+  // Suggest just passing in documentName alone if that's the only property used in this component.
+  document: documentType
 };
 
 export default DocumentActions;
