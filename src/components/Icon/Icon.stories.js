f410739de332f4d81f3f9e93cf587a6513106459
Cory House <housecor@gmail.com>
2019-05-11 21:00:55 -0500

    Add decorator for icon stories

--- src/components/Icon/Icon.stories.js
+++ src/components/Icon/Icon.stories.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Icon from "./Icon";
+import { host } from "storybook-host";
 
 storiesOf("Icons", module)
+  .addDecorator(
+    host({
+      title: "Icons",
+      align: "center top",
+      height: 500,
+      width: 400,
+      border: true
+    })
+  )
   .add("Info", () => <Icon type="info" />)
   .add("Warning", () => <Icon type="warning" />)
   .add("Error", () => <Icon type="error" />)
