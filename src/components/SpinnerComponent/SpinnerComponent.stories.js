ca50804218aec0ec6c004b94a02a980b38e9e494
John Cresswell <jcresswell@scriptpro.com>
2019-06-04 13:44:30 -0500

    Merged PR 6562: story for UI blocking
    story for UI blocking
    
    Related work items: #146299

--- src/components/SpinnerComponent/SpinnerComponent.stories.js
+++ src/components/SpinnerComponent/SpinnerComponent.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
 import SpinnerComponent from "./SpinnerComponent";
 
 storiesOf("Spinner Component", module)
@@ -10,4 +10,14 @@ storiesOf("Spinner Component", module)
   ))
   .add("Renders spinner with a custom message", () => (
     <SpinnerComponent message="This is a custom message" open={true} />
+  ))
+  .add("Blocks UI while active", () => (
+    <div>
+      <span>You should not be able to edit when spinner is active </span>
+      <br />
+      <span>Should be enabled if the spinner is off </span>
+      <br />
+      <input type="text" />
+      <SpinnerComponent open={boolean("Spinner on", true)} />
+    </div>
   ));
