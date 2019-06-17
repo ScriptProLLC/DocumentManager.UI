17958a7e191bf141151310992eee2f039d0157f2
John Cresswell <jcresswell@scriptpro.com>
2019-06-17 15:46:46 -0500

    Merged PR 6693: revised tests
    revised tests
    
    Related work items: #147372

--- src/components/DocumentManager/DocumentManager.cypress.js
+++ src/components/DocumentManager/DocumentManager.cypress.js
@@ -34,4 +34,92 @@ describe("DocumentManager", function() {
       ".document-list-item-container.selected .document-list-item-name span"
     ).should("contain.text", "Document 2");
   });
+
+  it("should show Loading spinner when a collection is loading", function() {
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    cy.visit(`?collection=${this.collectionId}`);
+
+    // should have a visible spinner
+    cy.get("[data-testid=spinner]").should("be.visible");
+  });
+
+  it("should hide Loading spinner when an empty collection is loaded", function() {
+    cy.visit(`?collection=${this.collectionId}`);
+
+    // spinner should immediate disappear on empty collection
+    cy.get("[data-testid=spinner]").should("not.exist");
+  });
+
+  it("should show Loading spinner when auto-selected document is loading", function() {
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    cy.visit(`?collection=${this.collectionId}`);
+
+    // we should have items in the list but nothing selected
+    cy.get("[data-testid=document_list_items]").should("exist");
+
+    // should appear when document selected
+    cy.get("[data-testid=spinner]").should("be.visible");
+  });
+
+  it("should hide Loading spinner after auto-selected document is loaded", function() {
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    cy.visit(`?collection=${this.collectionId}`);
+
+    // viewer state when DOM loaded but no loaded document
+    cy.get("[data-testid=document_viewer_no_documents]").should("exist");
+
+    // should appear when document loading
+    cy.get("[data-testid=spinner]").should("be.visible");
+
+    // viewer state when document loaded
+    cy.get("[data-testid=document_viewer_container_withdocs]").should("exist");
+
+    // should ago away after loading
+    cy.get("[data-testid=spinner]").should("not.exist");
+  });
+
+  it("should show Loading spinner when manually selected document is loading", function() {
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    cy.visit(`?collection=${this.collectionId}`);
+
+    // wait until spinner goes away
+    cy.get("[data-testid=spinner]").should("not.exist");
+
+    // Click the bottom item in the document list
+    cy.get("[data-testid=document_name]")
+      .last()
+      .click();
+
+    // spinner should be present after clicking
+    cy.get("[data-testid=spinner]").should("be.visible");
+  });
+
+  it("should hide Loading spinner after a manually selected document is loaded", function() {
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    cy.visit(`?collection=${this.collectionId}`);
+
+    // wait until spinner goes away
+    cy.get("[data-testid=spinner]").should("not.exist");
+
+    // Click the bottom item in the document list
+    cy.get("[data-testid=document_name]")
+      .last()
+      .click();
+
+    // viewer state when document loaded
+    cy.get("[data-testid=document_viewer_container_withdocs]").should("exist");
+
+    // should ago away after loading
+    cy.get("[data-testid=spinner]").should("not.exist");
+  });
 });
