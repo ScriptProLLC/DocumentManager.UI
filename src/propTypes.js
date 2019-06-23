1656278aa701c7a55170bf35e5cd383e68741e4a
Cory House <housecor@gmail.com>
2019-06-23 12:15:05 -0500

    Require proptypes

--- src/propTypes.js
+++ src/propTypes.js
@@ -1,10 +1,10 @@
 import { string, shape, object } from "prop-types";
 
 export const documentPropType = shape({
-  id: string,
-  documentCollectionId: string,
-  name: string,
-  documentFile: string,
-  dateCreated: string,
-  attributes: object
+  id: string.isRequired,
+  documentCollectionId: string.isRequired,
+  name: string.isRequired,
+  documentFile: string.isRequired,
+  dateCreated: string.isRequired,
+  attributes: object.isRequired
 });
