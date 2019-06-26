caae0315ccafbb8b56187216aa29025b4cafd90a
Russell Bronston <rbronston@scriptpro.com>
2019-06-26 13:46:20 -0500

    Merged PR 6781: Cypress test for Error Modal
    Cypress test for Error Modal this is for 148319 not 148349
    
    Related work items: #148319

--- src/components/ModalDialog/ModalDialog.cypress.js
+++ src/components/ModalDialog/ModalDialog.cypress.js
@@ -45,4 +45,158 @@ describe("ModalDialog", function() {
     // Verify the cancel button has focus
     cy.focused().should("have.attr", "data-testid", "modal_cancel_prompt");
   });
+
+  it("shows error retrieving document list", function() {
+    // Open Application to the collection
+    cy.visit(`?collection=${"invalid collection Id"}`);
+
+    // Verify "Error retrieving document list" displays
+    cy.get("[data-testid=modal_prompt").should(
+      "contain",
+      "Error retrieving document list"
+    );
+  });
+
+  it("shows error retrieving document", function() {
+    // Add new documents to collection
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    // // Open Application to the collection
+    cy.visit(`?collection=${this.collectionId}`);
+
+    cy.server();
+
+    cy.route({
+      method: "GET",
+      url: `**/documents/*`,
+      status: 404,
+      response: {},
+      headers: {
+        "x-correlation-id": "867-5309"
+      }
+    });
+
+    cy.get("[data-testid=document_name]")
+      .last()
+      .click({
+        force: true
+      });
+
+    // Verify "Error Retrieving Document" displays
+    cy.get("[data-testid=modal_prompt").should(
+      "contain",
+      "Error retrieving document"
+    );
+  });
+
+  it("shows error scanning document", function() {
+    // // Open Application to the collection
+    cy.visit(`?collection=${this.collectionId}`);
+
+    cy.server();
+
+    cy.route({
+      method: "GET",
+      url: `**/scan`,
+      status: 404,
+      response: {},
+      headers: {
+        "x-correlation-id": "867-5309"
+      }
+    });
+
+    cy.get("[data-testid=scan_icon_button]").click();
+
+    // Verify "Error scanning Document' displays
+    cy.get("[data-testid=modal_prompt").should(
+      "contain",
+      "Error scanning document"
+    );
+  });
+
+  it("shows error saving document", function() {
+    // // Open Application to the collection
+    cy.visit(`?collection=${this.collectionId}`);
+
+    cy.server();
+
+    cy.route({
+      method: "POST",
+      url: `**/documents*`,
+      status: 404,
+      response: {},
+      headers: {
+        "x-correlation-id": "867-5309"
+      }
+    });
+
+    cy.get("[data-testid=scan_icon_button]").click();
+
+    cy.get("[data-testid=save_button]").click();
+
+    // Verify "Error scanning Document" displays
+    cy.get("[data-testid=modal_prompt").should(
+      "contain",
+      "Error saving document"
+    );
+  });
+
+  it("shows error deleting document", function() {
+    // Add new documents to collection
+    cy.addDocumentToCollection(mockData.documents[1], this.collectionId);
+    cy.addDocumentToCollection(mockData.documents[0], this.collectionId);
+
+    // // Open Application to the collection
+    cy.visit(`?collection=${this.collectionId}`);
+
+    cy.server();
+
+    cy.route({
+      method: "DELETE",
+      url: `**/documents/*`,
+      status: 404,
+      response: {},
+      headers: {
+        "x-correlation-id": "867-5309"
+      }
+    });
+
+    cy.get("[data-testid=document_name]")
+      .last()
+      .click({ force: true });
+
+    cy.get("[data-testid=delete_button]").click();
+
+    cy.get("[data-testid=modal_confirm_prompt]").click();
+
+    // Verify "Error deleting Document" displays
+    cy.get("[data-testid=modal_prompt").should(
+      "contain",
+      "Error deleting document"
+    );
+  });
+
+  it("error dialog shows correlation id", function() {
+    cy.server();
+
+    cy.route({
+      method: "GET",
+      url: `**/collections/**`,
+      status: 404,
+      response: {},
+      headers: {
+        "x-correlation-id": "867-5309"
+      }
+    });
+
+    // Open Application to the bad collection
+    cy.visit(`?collection=${"invalid-collection-Id"}`);
+
+    // Verify Correlation ID Exists
+    cy.get("[data-testid=modal_prompt").should(
+      "contain",
+      "Log Correlation ID: 867-5309"
+    );
+  });
 });
